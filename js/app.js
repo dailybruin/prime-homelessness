d8b3bae272c92145f23d2e9ba2a565bd6c8a38c3
Katie <kaspii@users.noreply.github.com>
2016-02-20 11:01:30 -0800

    Made contents toggle with hover instead of click

--- js/app.js
+++ js/app.js
@@ -1,14 +1,4 @@
 // $(function(){
-//   $('.contents').hide();
-
-//   $('.contents-btn').click(function() {
-//     $('.contents').toggle();
-//   });
-
-//   console.log("In toggle function");
-// });
-
-// $(function(){
 //   console.log("In resize function");
 //   if ($(window).width() > 700) {
 //     $.stratus({
@@ -23,38 +13,30 @@
 //   $('.flexslider').flexslider();
 // });
 
+// var adjust_music = function() {
+//   console.log("In resize function");
+//   if ($(window).width() > 700) {
+//     $.stratus({
+//       key: "8f41bb09d7580c54b24f5be783d2b6fb",
+//       links: "https://soundcloud.com/zedsdead/sets/somewhere-else-ep"
+//     });
+//   }
+//   else {
+//     $('.small-player').append('<iframe width="100%" height="450" scrolling="no" frameborder="no" src="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/243897832&amp;auto_play=false&amp;hide_related=false&amp;show_comments=true&amp;show_user=true&amp;show_reposts=false&amp;visual=true"></iframe>')
+//   }
 
-
-// var menu_toggle = function() {
-//   /* Push the body and the nav over by 285px over */
-//   $('.icon-menu').click(function() {
-//     $('.menu').animate({
-//       left: "0px"
-//     }, 200);
-
-//     $('body').animate({
-//       left: "285px"
-//     }, 200);
-//   });
-
-//   /* Then push them back */
-//   $('.icon-close').click(function() {
-//     $('.menu').animate({
-//       left: "-285px"
-//     }, 200);
-
-//     $('body').animate({
-//       left: "0px"
-//     }, 200);
-//   });
+//   $('.flexslider').flexslider();
 // };
 
 var contents_toggle = function() {
-  $('.contents').hide();
-
-  $('.contents-btn').click(function() {
+  $('.contents-btn').mouseenter(function() {
     console.log("Toggling");
-    $('.contents').toggle();
+    $('.contents').show();
+  });
+
+  $('.contents').mouseleave(function() {
+    console.log("Toggling");
+    $('.contents').hide();
   });
 
   $(document).click(function() {
@@ -62,8 +44,7 @@ var contents_toggle = function() {
   });
 
   console.log("In toggle function");
-
 };
 
-
+$('.contents').hide();
 $(document).ready(contents_toggle);
